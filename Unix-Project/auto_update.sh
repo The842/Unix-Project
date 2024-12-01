a2f8823b04af72892d19b046788eec3629e5fecb
Adlane05 <143427032+Adlane05@users.noreply.github.com>
2024-12-01 18:11:00 -0500

    Update auto_update.sh
    the nginx section is unnecessary the website updates fine without it and it causes issues because of sudo
    
    Signed-off-by: Adlane05 <143427032+Adlane05@users.noreply.github.com>

--- Unix-Project/auto_update.sh
+++ Unix-Project/auto_update.sh
@@ -16,13 +16,3 @@ else
   echo "$(date): Failed to pull updates from GitHub" >> auto_update.log
   exit 1
 fi
-
-# Restart Nginx web server
-sudo systemctl restart nginx
-
-# Check if Nginx restart was successful
-if [ $? -eq 0 ]; then
-  echo "$(date): Nginx restarted successfully" >> auto_update.log
-else
-  echo "$(date): Failed to restart Nginx" >> auto_update.log
-fi
